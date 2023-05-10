9c844f78462686c969883906df9b82a23bfccb8c
Alfi Ahsani Taqwim <alfiahsanitaqwim06@gmail.com>
2023-05-10 15:14:43 +0700

    using ajax

---
+++ process.php
@@ -0,0 +1,8 @@
+<tr>
+    <td><?= $_POST['nama']; ?></td>
+    <td><?= $_POST['email']; ?></td>
+    <td><?= $_POST['alamat']; ?></td>
+    <td><?= $_POST['jurusan']; ?></td>
+    <td><?= $_POST['tahun']; ?></td>
+</tr>
+   
